60e878eff042e56a09812a2cec1ef36d232b43d3
Akshay Garg <akshaygarg_90@yahoo.com>
2024-02-27 17:03:55 +0100

    Create ETL_V3.py

---
+++ ETL_V3.py
@@ -0,0 +1,43 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.python import PythonOperator
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['akshay.garg@atoss.com'],
+    'email_on_failure': false,
+    'email_on_retry': False,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=1),
+}
+with DAG(
+    'Personio_Example',
+    default_args=default_args,
+    description='Call the personio API',
+    schedule_interval=None,
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+    tags=['Integration'],
+) as dag:
+    t1 = SimpleHttpOperator(
+                    task_id='Read_Data',
+                    method='GET',
+                    http_conn_id='northwind',
+                    endpoint='northwind/northwind.svc/Customers?$format=json',
+                    headers={"Content-Type": "application/json"},
+                    dag=dag)
+    
+    def xcom_check(ds, **kwargs):
+        val = kwargs['ti'].xcom_pull(key='return_value', task_ids='Read_Data')
+        return f"xcom_check has: {kwargs['ti']} and it says: {val}"
+     
+    t2 = PythonOperator(
+        task_id='inspect_dog',
+        python_callable=xcom_check,
+        provide_context=True
+    )
+
+t1 >> t2
