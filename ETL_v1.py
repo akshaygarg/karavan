11836dcfaff34b8c7b1cbb2962e002a304ab8d9d
Akshay Garg <akshaygarg_90@yahoo.com>
2024-02-27 13:27:47 +0100

    Update ETL_v1.py

--- ETL_v1.py
+++ ETL_v1.py
@@ -6,69 +6,30 @@ from airflow.operators.python import PythonOperator
 import logging
 
 
-from airflow.decorators import dag, task
-@dag(
-    schedule=None,
-    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
-    catchup=False,
-    tags=["example"],
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2020, 2, 27),
+    'email': ['ss@asd.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 1,
+    'retry_delay': timedelta(seconds=5),
+}
+dag = DAG(
+    'dag1',
+    default_args=default_args,
+    description='A simple tutorial DAG',
+    schedule_interval='@daily',
 )
-def tutorial_taskflow_api_v1():
-    """
-    ### TaskFlow API Tutorial Documentation
-    This is a simple data pipeline example which demonstrates the use of
-    the TaskFlow API using three simple tasks for Extract, Transform, and Load.
-    Documentation that goes along with the Airflow TaskFlow API tutorial is
-    located
-    [here](https://airflow.apache.org/docs/apache-airflow/stable/tutorial_taskflow_api.html)
-    """
-    @task()
-    def extract():
-        """
-        #### Extract task
-        A simple Extract task to get data ready for the rest of the data
-        pipeline. In this case, getting data is simulated by reading from a
-        hardcoded JSON string.
-        """
-        outResp = SimpleHttpOperator(
-          task_id="extract_data",
-          http_conn_id='api_product',
-          method='GET',
-          response_filter=lambda response: json.dumps(response.text),
-          log_response=True,
-          xcom_push=True,
-          dag=dag  
-        )
+t2 = SimpleHttpOperator(
+    task_id='get_labrador',
+    method='GET',
+    http_conn_id='northwind',
+    endpoint='northwind/northwind.svc/Customers?$format=json',
+    headers={"Content-Type": "application/json"},
+    xcom_push=True,
+    dag=dag
+)
 
-        outResp()
-      
-        data_string = '{"1001": 301.27, "1002": 433.21, "1003": 502.22}'
 
-        order_data_dict = json.loads(data_string)
-        return order_data_dict
-    @task(multiple_outputs=True)
-    def transform(order_data_dict: dict):
-        """
-        #### Transform task
-        A simple Transform task which takes in the collection of order data and
-        computes the total order value.
-        """
-        total_order_value = 0
-
-        for value in order_data_dict.values():
-            total_order_value += value
-
-        return {"total_order_value": total_order_value}
-    @task()
-    def load(total_order_value: float):
-        """
-        #### Load task
-        A simple Load task which takes in the result of the Transform task and
-        instead of saving it to end user review, just prints it out.
-        """
-
-        print(f"Total order value is: {total_order_value:.2f}")
-    order_data = extract()
-    order_summary = transform(order_data)
-    load(order_summary["total_order_value"])
-tutorial_taskflow_api_v1()
