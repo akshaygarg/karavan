04aff055095885da71fe60e1c51a09009803c294
Akshay Garg <akshaygarg_90@yahoo.com>
2024-02-27 17:37:28 +0100

    Update ETL_V3.py

--- ETL_V3.py
+++ ETL_V3.py
@@ -36,6 +36,7 @@ with DAG(
     def xcom_check(ds, **kwargs):
         url = BaseHook.get_connection('northwind').get_uri() + '/northwind/northwind.svc/Customers?$format=json';
         print(url)
+        url = "https://services.odata.org/northwind/northwind.svc/Customers?$format=json"
 
         
         # A GET request to the API
